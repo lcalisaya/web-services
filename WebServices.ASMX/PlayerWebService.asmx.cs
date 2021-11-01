8da39b53172a4d8e251268f6cbfcca936d69db2c
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-11-01 17:01:22 -0300

    Agrego las restantes operaciones CRUD de jugadores en el servicio ASMX

--- WebServices.ASMX/PlayerWebService.asmx.cs
+++ WebServices.ASMX/PlayerWebService.asmx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
@@ -23,6 +24,58 @@ namespace WebServices.ASMX
         public List<Player> GetPlayers()
         {
             return db.Player.ToList();
+        }
+
+        [WebMethod]
+        public Player GetPlayer(int id)
+        {
+            return db.Player.FirstOrDefault(x => x.Id == id);
+        }
+
+        [WebMethod]
+        public Player AddPlayer(string fname, string lname, DateTime birth, string origin, string genre, string clubnow)
+        {
+            Player newPlayer = new Player() { FirstName = fname,
+                                              LastName = lname,
+                                              BirthDate = birth,
+                                              Nationality = origin,
+                                              Genre = genre,
+                                              ActualClub = clubnow };
+
+            Player player = db.Player.Add(newPlayer);
+            db.SaveChanges();
+            return player;
+        }
+
+
+        [WebMethod]
+        public Player UpdatePlayer(int id, string fname, string lname, DateTime birth, string origin, string genre, string clubnow)
+        {
+            Player playerInDB = GetPlayer(id);
+            playerInDB.FirstName = fname;
+            playerInDB.LastName = lname;
+            playerInDB.Genre = genre;
+            playerInDB.ActualClub = clubnow;
+            playerInDB.BirthDate = birth;
+            playerInDB.Nationality = origin;
+
+            db.Entry(playerInDB).State = EntityState.Modified;
+
+            db.SaveChanges();
+            return playerInDB;
+        }
+
+        [WebMethod]
+        public List<Player> DeletePlayer(int id)
+        {
+            var player = db.Player.FirstOrDefault(x => x.Id == id);
+            if (player != null)
+            {
+                db.Player.Remove(player);
+                db.SaveChanges();
+            }
+
+            return GetPlayers();
         }
     }
 }
