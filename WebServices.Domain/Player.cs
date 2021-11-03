a2a41f3227011ff6592d237d5a58934ab55dd758
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-11-03 16:46:34 -0300

    Se muestran los jugadores desde la vista del cliente web

---
+++ WebServices.Domain/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebServices.Domain
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Date of birth"), DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MMMM yyyy}")]
+        public DateTime? BirthDate { get; set; }
+        public string Nationality { get; set; }
+        [DisplayName("Plays in")]
+        public string ActualClub { get; set; }
+        public string Genre { get; set; }
+    }
+}
